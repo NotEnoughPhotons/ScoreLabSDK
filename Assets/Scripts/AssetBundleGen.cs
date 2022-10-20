88a590ae11711c6d9e2f368a457ae946344117fc
adamdev <hmullinsk@hotmail.com>
2022-10-20 19:55:48 -0400

    bundle generator and our first HUD prefab!

---
+++ Assets/Scripts/AssetBundleGen.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+public class AssetBundleGen : MonoBehaviour
+{
+	[MenuItem("Assets/Build Project Bundles")]
+	private static void BuildBundles()
+	{
+		BuildPipeline.BuildAssetBundles(Application.dataPath + "/AssetBundles/", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+	}
+}
+#endif
