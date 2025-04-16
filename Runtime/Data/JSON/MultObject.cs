e8919a14640a44577528d0fc209a7e6006b0a09b
adamdev <adamdevofficial@protonmail.com>
2025-04-16 12:17:12 -0400

    Cleaned up asset menu names for Score/Mult objects

--- Runtime/Data/JSON/MultObject.cs
+++ Runtime/Data/JSON/MultObject.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace NEP.ScoreLab.Data
 {
     [System.Serializable]
-    [CreateAssetMenu(fileName = "MultObject", menuName = "Not Enough Photons/ScoreLab/Create/Mult Object", order = 0)]
+    [CreateAssetMenu(fileName = "MultiplierObject", menuName = "Not Enough Photons/ScoreLab/Multiplier Object", order = 0)]
     public class MultObject : ScriptableObject
     {
         public JSONMult multiplier;
--- Runtime/Data/JSON/ScoreObject.cs
+++ Runtime/Data/JSON/ScoreObject.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace NEP.ScoreLab.Data
 {
     [System.Serializable]
-    [CreateAssetMenu(fileName = "ScoreObject", menuName = "Not Enough Photons/ScoreLab/Create/Score Object", order = 0)]
+    [CreateAssetMenu(fileName = "ScoreObject", menuName = "Not Enough Photons/ScoreLab/Score Object", order = 0)]
     public class ScoreObject : ScriptableObject
     {
         public JSONScore score;
