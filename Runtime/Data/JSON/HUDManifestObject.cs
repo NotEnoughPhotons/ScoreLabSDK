4a580428aa300e01c34fb20608a812d273903c99
adamdev <adamdevofficial@protonmail.com>
2025-04-16 14:34:06 -0400

    JSON serialization of a HUD manifest and GUID setting

--- Runtime/Data/JSON/HUDManifestObject.cs
+++ Runtime/Data/JSON/HUDManifestObject.cs
@@ -1,5 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
+using System.IO;
+using System.Security.Policy;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace NEP.ScoreLab.Data
@@ -8,5 +12,52 @@ namespace NEP.ScoreLab.Data
     public class HUDManifestObject : ScriptableObject
     {
         public JSONHUDManifest manifest;
+
+        public string ToJSON()
+        {
+            string json = "";
+            using (StringWriter sw = new StringWriter())
+            {
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    writer.Formatting = Formatting.Indented;
+                            
+                    writer.WriteStartObject();
+                    
+                    writer.WritePropertyName("name");
+                    writer.WriteValue(manifest.Name);
+                    writer.WritePropertyName("author");
+                    writer.WriteValue(manifest.Author);
+                    writer.WritePropertyName("description");
+                    writer.WriteValue(manifest.Description);
+
+                    if (manifest.Tags != null && manifest.Tags.Length > 0)
+                    {
+                        writer.WritePropertyName("tags");
+                        writer.WriteStartArray();
+                        for (int i = 0; i < manifest.Tags.Length; i++)
+                        {
+                            writer.WriteValue(manifest.Tags[i]);
+                        }
+                        writer.WriteEndArray();
+                    }
+                    
+                    writer.WritePropertyName("guid");
+                    writer.WriteValue(manifest.GUID);
+                            
+                    writer.WriteEndObject();
+                }
+
+                json = sw.ToString();
+            }
+
+            return json;
+        }
+
+        public void SetGUID(string guid)
+        {
+            manifest.GUID = guid;
+        }
     }
 }
+#endif
