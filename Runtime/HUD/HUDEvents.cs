b3f217080bf4c0cf03a1fa5ad9cbb3346dfa104f
adamdev <adamdevofficial@protonmail.com>
2025-06-16 15:14:22 -0400

    UltEvent support

---
+++ Runtime/HUD/HUDEvents.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+using UltEvents;
+    
+namespace NEP.ScoreLab.HUD
+{
+    public class HUDEvents : MonoBehaviour
+    {
+        public UltEventHolder OnScoreUpdateEvent;
+        public UltEventHolder OnMultiplierUpdateEvent;
+    }
+}
