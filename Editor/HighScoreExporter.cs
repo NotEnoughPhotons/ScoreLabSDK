8cd1f35271a4088d4864f69d720ece2c248cefc7
adamdev <adamdevofficial@protonmail.com>
2025-07-14 00:34:38 -0400

    High score exporter and definition file

---
+++ Editor/HighScoreExporter.cs
@@ -0,0 +1,151 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using NEP.ScoreLab.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace NEP.ScoreLab.Editor
+{
+    public class HighScoreExporter : EditorWindow
+    {
+        [SerializeField] List<HighScoreDefinition> m_definitions = new();
+        private string m_exportLocation;
+        
+        [MenuItem("Not Enough Photons/ScoreLab/Export High Score Definitions", false, 10)]
+        public static void ShowWindow()
+        {
+            EditorWindow window = GetWindow(typeof(HighScoreExporter));
+            window.titleContent = new GUIContent("High Score Exporter");
+        }
+
+        private void OnGUI()
+        {
+            ScriptableObject target = this;
+            SerializedObject serializedObject = new SerializedObject(target);
+            SerializedProperty definitions = serializedObject.FindProperty("m_definitions");
+            
+            EditorGUILayout.PropertyField(definitions, true);
+            serializedObject.ApplyModifiedProperties();
+            
+            if (m_definitions == null || m_definitions.Count == 0)
+            {
+                EditorGUILayout.HelpBox(
+                    "At least one high score definition is required!",
+                    MessageType.Error);
+                return;
+            }
+            
+            m_exportLocation = EditorGUILayout.TextField("Export Location:", m_exportLocation);
+
+            if (GUILayout.Button("Export"))
+            {
+                WriteJSONDefinition(m_definitions.ToArray());
+                CleanupBuildDirectory(m_exportLocation);
+            }
+        }
+
+        private void WriteJSONDefinition(HighScoreDefinition[] definitions)
+        {
+            if (definitions == null || definitions.Length == 0)
+            {
+                return;
+            }
+
+            string exportPath = Path.Combine(m_exportLocation, "hstable.json");
+
+            Directory.CreateDirectory(m_exportLocation);
+
+            using (StreamWriter sw = new StreamWriter(exportPath))
+            {
+                using (JsonTextWriter writer = new JsonTextWriter(sw))
+                {
+                    writer.Formatting = Formatting.Indented;
+                    
+                    writer.WriteStartObject();
+                    writer.WritePropertyName("table");
+                    writer.WriteStartArray();
+                    foreach (var definition in definitions)
+                    {
+                        JObject obj = definition.ToJson();
+                        obj.WriteTo(writer);
+                    }
+                    writer.WriteEndArray();
+                    writer.WriteEndObject();
+                }
+            }
+        }
+        
+        private void CleanupBuildDirectory(string directory)
+        {
+            if (directory == string.Empty)
+            {
+                return;
+            }
+
+            string[] files = Directory.GetFiles(directory);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file = files[i];
+
+                bool isBlacklisted = file.EndsWith(".manifest") || file.EndsWith(".meta");
+                
+                if (isBlacklisted)
+                {
+                    // NOTE: This is pretty dangerous. Must replace with something safer.
+                    File.Delete(file);
+                }
+                
+                // Typically when a HUD is built, there's a file without an extension.
+                // This file is named after whatever the parent folder is named.
+                // The file has a magic header named "UnityFS".
+
+                DirectoryInfo parentDirectory = Directory.GetParent(file);
+                
+                if (parentDirectory == null)
+                {
+                    continue;
+                }
+
+                if (!file.EndsWith(parentDirectory.Name))
+                {
+                    continue;
+                }
+                
+                bool isCorrectFile = false;
+                
+                using (FileStream stream = new FileStream(file, FileMode.Open))
+                {
+                    using (BinaryReader reader = new BinaryReader(stream))
+                    {
+                        char[] chars = reader.ReadChars(7);
+                        
+                        // Have to use a string builder here, otherwise -
+                        // System.Chars[] will get outputted if I try to print it normally.
+                        // Stupid.
+                        StringBuilder builder = new StringBuilder();
+                        builder.Append(chars);
+
+                        string magic = builder.ToString();
+
+                        // Update the flag so we know it's the correct file
+                        if (magic == "UnityFS")
+                        {
+                            isCorrectFile = true;
+                        }
+                    }
+                }
+
+                if (isCorrectFile)
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+}
+#endif
---
+++ Runtime/Data/JSON/HighScoreDefinition.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace NEP.ScoreLab.Data
+{
+    [System.Serializable]
+    [CreateAssetMenu(fileName = "High Score Definition", menuName="Not Enough Photons/ScoreLab/New High Score Definition", order = 20)]
+    public class HighScoreDefinition : ScriptableObject
+    {
+        [System.Serializable]
+        public class GradeDefinition
+        {
+            public string Grade => _grade;
+            public int Threshold => _threshold;
+        
+            [SerializeField] private string _grade;
+            [SerializeField] private int _threshold;
+
+            public JObject ToJSON()
+            {
+                JObject result = null;
+
+                using (JTokenWriter writer = new JTokenWriter())
+                {
+                    writer.WriteStartObject();
+                    writer.WritePropertyName("grade");
+                    writer.WriteValue(_grade);
+                    writer.WritePropertyName("threshold");
+                    writer.WriteValue(_threshold);
+                    writer.WriteEndObject();
+
+                    result = writer.Token as JObject;
+                }
+                
+                return result;
+            }
+        }
+        
+        public string Barcode => _barcode;
+        public GradeDefinition[] Grades => _grades;
+        public int Score => _score;
+        public bool IsBaseGame => _isBaseGame;
+        
+        [SerializeField] private string _barcode;
+        [SerializeField] private GradeDefinition[] _grades;
+        [SerializeField] private int _score;
+        [SerializeField] private bool _isBaseGame;
+
+        public JObject ToJson()
+        {
+            JObject result = null;
+
+            using (JTokenWriter writer = new JTokenWriter())
+            {
+                writer.Formatting = Formatting.Indented;
+                    
+                writer.WriteStartObject();
+                writer.WritePropertyName("barcode");
+                writer.WriteValue(_barcode);
+                if (_grades != null)
+                {
+                    writer.WritePropertyName("grades");
+                    writer.WriteStartArray();
+                    JArray array = writer.CurrentToken as JArray;
+                    for (int i = 0; i < _grades.Length; i++)
+                    {
+                        JObject grade = _grades[i].ToJSON();
+                        array.Add(grade);
+                    }
+                    writer.WriteEndArray();
+                }
+                else
+                {
+                    writer.WritePropertyName("score");
+                    writer.WriteValue(_score);
+                }
+                writer.WriteEndObject();
+
+                result = writer.Token as JObject;
+            }
+
+            return result;
+        }
+    }
+}
