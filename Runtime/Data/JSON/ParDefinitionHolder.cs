1f7bdee81b2b671d1d0ed4cafd41ec77215dc2aa
adamdev <adamdevofficial@protonmail.com>
2025-08-09 17:38:55 -0400

    Par definition holder component

---
+++ Runtime/Data/JSON/ParDefinitionHolder.cs
@@ -0,0 +1,10 @@
+using NEP.ScoreLab.Data;
+using UnityEngine;
+
+namespace NEP.ScoreLab.SDK
+{
+    public class ParDefinitionHolder : MonoBehaviour
+    {
+        [SerializeField] private ParDefinition _parDefinition;
+    }
+}
